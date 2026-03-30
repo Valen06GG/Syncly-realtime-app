b649116d40730f19f19328e85d5ff967683a8f8d
Valentin Fortunato <valenfortunato06@gmail.com>
2026-03-30 16:19:21 -0300

    Tercer Commit / Syncly

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
+import { TasksModule } from './tasks/tasks.module';
+import { SocketsModule } from './sockets/sockets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Valen06GG',
+      database: 'syncly',
+      autoLoadEntities: true,
+      synchronize: true, 
+    }),
+    AuthModule,
+    UsersModule,
+    BoardsModule,
+    ColumnsModule,
+    TasksModule,
+    SocketsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
